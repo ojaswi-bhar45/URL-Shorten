1f0506a7c9c557cb27fd25dbf2829494c4f6be1a
Ojaswi bhardwaj <ojaswibhardwaj86@gmail.com>
2026-07-20 18:16:03 +0530

    refactor health check response and ensure middleware usage

--- url-shorten-b/app.js
+++ url-shorten-b/app.js
@@ -3,10 +3,13 @@ const app = express();
 
 let port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
 
 app.get("/health", (req, res) => {
-  res.send("Ok");
+  res.json("OK");
 });
