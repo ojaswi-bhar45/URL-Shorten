3bbe23c2ece5c54c5aa13eb06196ba4eb107eb34
Ojaswi bhardwaj <ojaswibhardwaj86@gmail.com>
2026-07-24 17:59:11 +0530

    refactor: improve error handling and serialization in URL shortening logic

--- url-shorten-b/app.js
+++ url-shorten-b/app.js
@@ -2,36 +2,52 @@ require("dotenv").config();
 const express = require("express");
 const prisma = require("./prismaclient");
 const { shortenSchema } = require("./urlSchema");
+const { customAlphabet } = require("nanoid");
 
 const app = express();
 
 let port = 3000;
 
+function serializeBigInt(obj) {
+  return JSON.parse(
+    JSON.stringify(obj, (key, value) =>
+      typeof value === "bigint" ? value.toString() : value,
+    ),
+  );
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const nanoid = customAlphabet(
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+  7,
+);
 
 app.get("/health", async (req, res) => {
   try {
     await prisma.$queryRaw`SELECT 1`;
     res.json({ messsage: "DataBase connected" });
   } catch (err) {
+    console.error("Prisma error:", err);
     res.json(err);
   }
 });
 
 app.post("/shorten", async (req, res) => {
-  const result = shortenSchema.safeParse(req.body);
+  let result = shortenSchema.safeParse(req.body);
 
   if (!result.success) {
     return res.status(400).json({ error: result.error.issues[0].message });
   }
 
   try {
-    const url = await prisma.url.create({
-      data: { longUrl: result.data.url },
+    let url = await prisma.url.create({
+      data: { longUrl: result.data.url, shortCode: nanoid() },
     });
-    res.status(201).json(url);
+    res.status(201).json(serializeBigInt(url));
   } catch (err) {
+    console.error("Prisma error:", err); // add this
     res.status(500).json({ error: "Failed to create short URL" });
   }
 });
