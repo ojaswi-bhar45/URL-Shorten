a774dcb9bf3706cafacb5859895166ae54f7fcd3
Ojaswi bhardwaj <ojaswibhardwaj86@gmail.com>
2026-08-12 09:23:53 +0530

    feat: add Redis and Prisma database integration; remove unused serialize utility

--- url-shorten-b/utils/serialize.js
+++
@@ -1,9 +0,0 @@
-function serializeBigInt(obj) {
-  return JSON.parse(
-    JSON.stringify(obj, (key, value) =>
-      typeof value === "bigint" ? value.toString() : value,
-    ),
-  );
-}
-
-module.exports = { serializeBigInt };
